26568d61bfe6c9f45431cb2e8c0d2a70845246b8
veio007 <veio007@163.com>
2017-08-21 15:11:16 +0800

    update

--- app.js
+++
@@ -1,7 +0,0 @@
-var connect = require("connect");
-var serveStatic = require("serve-static");
-
-var app = connect();
-app.use(serveStatic("."));
-app.listen(8086);
-console.log("server starting in port 8086")
